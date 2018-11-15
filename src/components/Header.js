4e5850c6e0d285fe42a9347b3f5c908caf1629dc
Jake Witcher <jjwitcher09@gmail.com>
2018-11-15 00:04:13 -0500

    add init routes

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active" exact={true}>Create expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active" exact={true}>Edit expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active" exact={true}>Help page</NavLink>
+    </header>
+);
+
+export default Header; 

3a19e12f3f215f4874b10581dda50f2d58c023f8
Jake Witcher <jjwitcher09@gmail.com>
2018-11-18 22:49:26 -0500

    add ExpenseList and ExpenseListItem

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -21,4 +22,10 @@ store.dispatch(addExpense({ description: 'Gas bill', amount: 5300, createdAt: 27
 
 store.dispatch(setTextFilter('water'));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));

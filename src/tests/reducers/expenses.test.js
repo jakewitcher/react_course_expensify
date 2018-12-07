cfe6d0bb09b6a7eae54b9bb63045f36dbd53f5f5
Jake Witcher <jjwitcher09@gmail.com>
2018-12-06 22:06:18 -0500

    add tests for expenses and filters reducers

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,62 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id,
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENS',
+        id: '-1',
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('should add expense', () => {
+    const expense = {
+        id: 4,
+        description: 'hulu',
+        note: '',
+        amount: 99,
+        createdAt: 1000,
+    };
+    const action = { type: 'ADD_EXPENSE', expense };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, expense]);
+});
+
+test('should edit an expense', () => {
+    const updates = {
+        description: 'cell phone',
+        amount: 1000,
+        createdAt: 2000,
+    };
+    const action = { type: 'EDIT_EXPENSE', id: expenses[0].id, updates };
+    const state = expensesReducer(expenses, action);
+    expect(state[0].description).toEqual('cell phone');
+    expect(state[0].amount).toEqual(1000);
+    expect(state[0].createdAt).toEqual(2000);
+});
+
+test('should not edit expense if expense not found', () => {
+    const updates = {
+        description: 'cell phone',
+        amount: 1000,
+        createdAt: 2000,
+    };
+    const action = { type: 'EDIT_EXPENSE', id: -1, updates };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});

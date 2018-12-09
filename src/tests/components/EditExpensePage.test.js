737f81101f9382d3de3e22f5f60f533de9a01c02
Jake Witcher <jjwitcher09@gmail.com>
2018-12-08 22:28:22 -0500

    add tests for ExpenseListFilters component

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -31,7 +31,7 @@ test('should handle editExpense', () => {
 });
 
 test('should handle removeExpense', () => {
-    wrapper.find('button').prop('onClick')();
+    wrapper.find('button').simulate('click');
     expect(history.push).toHaveBeenLastCalledWith('/');
     expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[2].id});
 });

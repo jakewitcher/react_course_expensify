839ee6bea8d58b034d601d0a9e2fe9da639aa91a
Jake Witcher <jjwitcher09@gmail.com>
2018-12-12 22:54:03 -0500

    fix typo in AppRouter file, change DashBoard to Dashboard

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import ExpenseDashboardPage from '../components/ExpenseDashBoardPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';

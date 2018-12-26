45c919e66fbf936aef61dad70a45e7d11951e4f4
Jake Witcher <jjwitcher09@gmail.com>
2018-12-26 17:49:48 -0500

    style search expense filters page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -66,6 +66,7 @@ export default class ExpenseForm extends React.Component {
                 {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.onSubmit}>
                     <input
+                        className="text-input"
                         type="text"
                         placeholder="Description"
                         autoFocus
@@ -73,6 +74,7 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onDescriptionChange}
                     />
                     <input
+                        className="text-input"
                         type="text"
                         placeholder="Amount"
                         value={this.state.amount}
@@ -87,6 +89,7 @@ export default class ExpenseForm extends React.Component {
                         isOutsideRange={(day) => false}
                     />
                     <textarea
+                        className="textarea"
                         placeholder="Add a note for your expense (optional)"
                         value={this.state.note}
                         onChange={this.onNoteChange}

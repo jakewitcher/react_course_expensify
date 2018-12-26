d494caae8ff77cee470a983dfe973a4cb0b36597
Jake Witcher <jjwitcher09@gmail.com>
2018-12-26 16:08:08 -0500

    style login page and header, update tests

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -8,8 +9,13 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const amount = numeral(expensesTotal / 100).format('$0,0.00');
     const isPlural = expensesCount === 1 ? 'expense' : 'expenses';
     return (
-        <div>
-            <h1>{`Viewing ${expensesCount} ${isPlural} totaling ${amount}`}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {isPlural} totaling <span>{amount}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to ="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
